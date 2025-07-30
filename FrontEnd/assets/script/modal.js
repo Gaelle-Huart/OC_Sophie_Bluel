b0e15e7b774c3ef1ad0e11ad231950ba08e31025
Gaelle Huart <gaelle.c.huart@gmail.com>
2025-07-30 21:48:05 +0200

    Retravail de la modale (code JavaScript amélioré, balise HTML <dialog> mieux implémentée).
    Affinage de l'accessibilité (le focus avec CSS et
    la touche Tab dans la modale, fermer la modale en
    appuyant sur Echap et clic dans le backdrop).

--- FrontEnd/assets/script/modal.js
+++ FrontEnd/assets/script/modal.js
@@ -1,5 +1,5 @@
 /////// ///// ////
-///// ///// //// //// /// /// // // // Code dédié à la modale // // // /// /// //// //// ///// /////
+///// ///// //// //// /// /// // // // // // // Code dédié à la modale // // // // // // /// /// //// //// ///// /////
 /// ///// ////
 
 let modal = null;
@@ -15,88 +15,148 @@ const close = document.querySelector(".topIcons_close");                  /// //
 const modalWrap = document.querySelector(".modal_wrapper");               /// /// identifie l'extérieur des modales /// ///
 const modalBox = document.querySelector(".modal");                        /// /// identifie la zone de contenu des modales /// ///
 const focusTargets = 'button, a, input, textarea, select';                /// /// détermine les éléments cibles du focus /// ///
-const focusGallery = modalGallery.querySelectorAll(focusTargets);         /// /// identifie les cibles du focus (galerie) /// ///
-const focusAdd = modalAdd.querySelectorAll(focusTargets);                 /// /// identifie les cibles du focus dans l'ajout photo /// ///
-const focusGalArray = Array.from(focusGallery);                           /// /// crée un tableau des cibles du focus (galerie) /// ///
-const focusAddArray = Array.from(focusAdd);                               /// /// crée un tableau des cibles du focus (ajout) /// ///
 
-///// ///// //// //// /// /// // // // ouverture et fermeture de la modale "Galerie" // // // /// /// //// //// ///// /////
-modify.addEventListener("click", () => {
-  modalGallery.classList.remove("hidden");
-  modalGallery.setAttribute("aria-hidden", "false");
-  modalGallery.setAttribute("aria-modal", "true");
-  modal = modalGallery ;
-});
 
-closeGallery.addEventListener("click", () => {
-  modalGallery.classList.add("hidden");
-  modalGallery.setAttribute("aria-hidden", "true");
-  modalGallery.removeAttribute("aria-modal");
-  modal = null ;
-});
-
-///// ///// //// //// /// /// // // // fermeture des modales au clic dans l'arrière plan // // // /// /// //// //// ///// /////
-modalGallery.addEventListener("click", (e) => {
-  if(e.target === modalGallery) {
-    modalGallery.classList.add("hidden");
-    modalGallery.setAttribute("aria-hidden", "true");
-    modalGallery.removeAttribute("aria-modal");
-    modal = null ;
-  }
-  if(e.target === modalAdd) {
-    modalAdd.classList.add("hidden");
-    modalAdd.setAttribute("aria-hidden", "true");
-    modalAdd.removeAttribute("aria-modal");
-    modal = null ;
-  }
-});
-
-///// ///// //// //// /// /// // // // gestion de la touche "Tab" à l'intérieur des modales // // // /// /// //// //// ///// /////
-document.addEventListener("keydown", (e) => {
-  if (e.key === "Tab" && modal === modalGallery) {                        /// /// si Tab est utilisé et que la modale est ouverte /// ///
-    const focusStart = focusGalArray[0];                                  /// /// début des éléments prenant le focus /// ///
-    const focusEnd = focusGalArray[focusGalArray.length - 1];             /// /// fin des éléments prenant le focus /// ///
-    if (e.shiftKey) { 
-      if (document.activeElement === focusStart) {
-        e.preventDefault();
-        focusEnd.focus();
-      }
-    } else { 
-      if (document.activeElement === focusEnd) {
-        e.preventDefault();
-        focusStart.focus(); 
+function openModal() {
+  modify.addEventListener("click", (e) => {
+    if (e.target === document.querySelector(".modify")) {
+      modal = modalGallery;
+      modal.style.display = "flex";
+      modal.setAttribute("aria-hidden", "false");
+      modal.setAttribute("aria-modal", "true");
+      modal.showModal();
+    }
+    noCloseInBox();
+  });
+  closeGallery.addEventListener("click", closeModal);
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape" && modal != null) {
+      closeModal();
+    }
+  });
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "Tab" && modal != null) {                               /// /// si Tab est utilisé et que la modale est ouverte /// ///
+      const focusIn = modal.querySelectorAll(focusTargets);               /// /// identifie les cibles du focus /// ///
+      const focusArray = Array.from(focusIn);                             /// /// crée un tableau des cibles du focus /// ///
+      const focusStart = focusArray[0];                                   /// /// début des éléments prenant le focus /// ///
+      const focusEnd = focusArray[focusArray.length - 1];                 /// /// fin des éléments prenant le focus /// ///
+      if (e.shiftKey) { 
+        if (document.activeElement === focusStart) {
+          e.preventDefault();
+          focusEnd.focus();
+        }
+      } else { 
+        if (document.activeElement === focusEnd) {
+          e.preventDefault();
+          focusStart.focus(); 
+        }
       }
     }
-  }
-  if (e.key === "Tab" && modal === modalAdd) {                            /// /// si Tab est utilisé et que la modale est ouverte /// ///
-    const focusStart = focusAddArray[0];                                  /// /// début des éléments prenant le focus /// ///
-    const focusEnd = focusAddArray[focusAddArray.length - 1];             /// /// fin des éléments prenant le focus /// ///
-    if (e.shiftKey) { 
-      if (document.activeElement === focusStart) {
-        e.preventDefault();
-        focusEnd.focus();
-      }
-    } else { 
-      if (document.activeElement === focusEnd) {
-        e.preventDefault();
-        focusStart.focus(); 
-      }
-    }
-  }
-});
+  });
+};
+openModal();
 
-///// ///// //// //// /// /// // // // support de la touche "Escape" // // // /// /// //// //// ///// /////
-document.addEventListener("keydown", (e) => {
-  if (e.key === "Escape" && modal === modalGallery) {
-    modalGallery.classList.add("hidden");
-    modalGallery.setAttribute("aria-hidden", "true");
-    modalGallery.removeAttribute("aria-modal");
-    modal = null ;
-  };
-  if (e.key === "Escape" && modal === modalAdd) {
-    modalAdd.classList.add("hidden");
-    modalAdd.setAttribute("aria-hidden", "true");
-    modalAdd.removeAttribute("aria-modal");
-    modal = null ;
-  };
-});
+function closeModal() {
+  if (!modal) return;
+  modal.style.display = "";
+  modal.setAttribute("aria-hidden", "true");
+  modal.removeAttribute("aria-modal");
+  modal.close();
+};
+
+function noCloseInBox() {
+  if(modal === modalGallery || modal === modalAdd) {
+    modal.addEventListener("click", (event) => {
+      const rect = modal.getBoundingClientRect();                           /// /// exclu la zone du contenu de la fermeture au clic /// ///
+        const isInDialog = (
+        event.clientX >= rect.left &&
+        event.clientX <= rect.right &&
+        event.clientY >= rect.top &&
+        event.clientY <= rect.bottom
+      );
+      if (!isInDialog) {
+        closeModal();
+      }
+    });
+  }
+};
+// ///// ///// //// //// /// /// // // // ouverture et fermeture de la modale "Galerie" // // // /// /// //// //// ///// /////
+// modify.addEventListener("click", () => {
+//   modalGallery.classList.remove("hidden");
+//   modalGallery.setAttribute("aria-hidden", "false");
+//   modalGallery.setAttribute("aria-modal", "true");
+//   modal = modalGallery ;
+// });
+
+// closeGallery.addEventListener("click", () => {
+//   modalGallery.classList.add("hidden");
+//   modalGallery.setAttribute("aria-hidden", "true");
+//   modalGallery.removeAttribute("aria-modal");
+//   modal = null ;
+// });
+
+// ///// ///// //// //// /// /// // // // fermeture des modales au clic dans l'arrière plan // // // /// /// //// //// ///// /////
+// modalGallery.addEventListener("click", (e) => {
+//   if(e.target === modalGallery) {
+//     modalGallery.classList.add("hidden");
+//     modalGallery.setAttribute("aria-hidden", "true");
+//     modalGallery.removeAttribute("aria-modal");
+//     modal = null ;
+//   }
+//   if(e.target === modalAdd) {
+//     modalAdd.classList.add("hidden");
+//     modalAdd.setAttribute("aria-hidden", "true");
+//     modalAdd.removeAttribute("aria-modal");
+//     modal = null ;
+//   }
+// });
+
+// ///// ///// //// //// /// /// // // // gestion de la touche "Tab" à l'intérieur des modales // // // /// /// //// //// ///// /////
+// document.addEventListener("keydown", (e) => {
+//   if (e.key === "Tab" && modal === modalGallery) {                        /// /// si Tab est utilisé et que la modale est ouverte /// ///
+//     const focusStart = focusGalArray[0];                                  /// /// début des éléments prenant le focus /// ///
+//     const focusEnd = focusGalArray[focusGalArray.length - 1];             /// /// fin des éléments prenant le focus /// ///
+//     if (e.shiftKey) { 
+//       if (document.activeElement === focusStart) {
+//         e.preventDefault();
+//         focusEnd.focus();
+//       }
+//     } else { 
+//       if (document.activeElement === focusEnd) {
+//         e.preventDefault();
+//         focusStart.focus(); 
+//       }
+//     }
+//   }
+//   if (e.key === "Tab" && modal === modalAdd) {                            /// /// si Tab est utilisé et que la modale est ouverte /// ///
+//     const focusStart = focusAddArray[0];                                  /// /// début des éléments prenant le focus /// ///
+//     const focusEnd = focusAddArray[focusAddArray.length - 1];             /// /// fin des éléments prenant le focus /// ///
+//     if (e.shiftKey) { 
+//       if (document.activeElement === focusStart) {
+//         e.preventDefault();
+//         focusEnd.focus();
+//       }
+//     } else { 
+//       if (document.activeElement === focusEnd) {
+//         e.preventDefault();
+//         focusStart.focus(); 
+//       }
+//     }
+//   }
+// });
+
+// ///// ///// //// //// /// /// // // // support de la touche "Escape" // // // /// /// //// //// ///// /////
+// document.addEventListener("keydown", (e) => {
+//   if (e.key === "Escape" && modal === modalGallery) {
+//     modalGallery.classList.add("hidden");
+//     modalGallery.setAttribute("aria-hidden", "true");
+//     modalGallery.removeAttribute("aria-modal");
+//     modal = null ;
+//   };
+//   if (e.key === "Escape" && modal === modalAdd) {
+//     modalAdd.classList.add("hidden");
+//     modalAdd.setAttribute("aria-hidden", "true");
+//     modalAdd.removeAttribute("aria-modal");
+//     modal = null ;
+//   };
+// });
